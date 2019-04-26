3876f7375cfd90a2999394fd680ad29a783ed596
jin <526302433@qq.com>
2019-04-26 11:52:20 +0800

    第九题：回文数
    耗时：54 ms
    内粗消耗：38.3MB

---
+++ isPalindrome.java
@@ -0,0 +1,9 @@
+    public boolean isPalindrome(int x) {
+         String in = String.valueOf(x);
+        int i = 0;
+        int j = in.length() - 1;
+        while (i < j) {
+            if (in.charAt(i++) != in.charAt(j--)) return false;
+        }
+        return true;
+    }
